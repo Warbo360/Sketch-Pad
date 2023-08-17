c515d000e7728d264076b36716492b509044c950
Warbo360 <129965715+Warbo360@users.noreply.github.com>
2023-08-17 04:24:17 -0500

    Added click and drag draw functionality
    This was done by making a variable either set to true or false based on
    if the mouse was held down and if true would use the draw mode selected
    to fill a div that has the mouse pointer over it to color that div as
    need.

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ function createCanvas(sizeInput) {
 
 createCanvas(prompt("Input width x height number up to 100 please")); // Calling the above function with the input being a prompt to generate the sketch grid
 
-let singleBox = document.querySelectorAll('.singleBox');
-let sketchSize = document.querySelectorAll('.sketchSize');
-let drawMode;
+let singleBox = document.querySelectorAll('.singleBox'); // selecting the divs in the drawing area
+let sketchSize = document.querySelectorAll('.sketchSize'); // selecting the div that will eventaully hold the various draw modes
+let drawMode; // variable that is used to set the drawmode, and if clear is selected loops over each div coloring each white
 
+
+// Event listener to set draw modes based on buttons selctions and a for loop if the clear option is selected colors all the canvas divs white
 sketchSize.forEach(((element) => element.addEventListener('click', function(){
     console.log(element);
     drawMode = element.innerText.toLowerCase();
@@ -29,7 +31,7 @@ sketchSize.forEach(((element) => element.addEventListener('click', function(){
     };
 })));
 
-
+// all of the draw modes, random integer function added to generate the random color for the rgb method
 function draw(e) {
     e.target.style.backgroundColor = 'black';
 };
@@ -46,18 +48,28 @@ function rgb(e) {
     e.target.style.backgroundColor = 'rgb' + '(' + getRandomInteger(255) + ',' + getRandomInteger(255) + ',' + getRandomInteger(255) + ')';
 };
 
+// variable and eventlistener to track if the mouse is held down and sets the variable to true of false to be used in the for loop below to actually draw in the canvas
+let isMouseDown = false;
+
+document.addEventListener('mousedown', function() {
+    isMouseDown = true;
+});
+
+document.addEventListener('mouseup', function() {
+    isMouseDown = false;
+})
+
+// if isMouseDown = true, then the for loop fills in a div that has the mouse over it allows to press and hold draw functionality and stops if the button is not held
 for (i = 0; i < singleBox.length; i++ ) {
-    singleBox[i].addEventListener('click', function(e) {
-        if (drawMode === 'draw') {
+    singleBox[i].addEventListener('pointerover', function(e) {
+        if (drawMode === 'draw' && isMouseDown === true) {
             draw(e);
-        } else if (drawMode === 'erase') {
+        } else if (drawMode === 'erase' && isMouseDown === true) {
             erase(e);
-        } else if (drawMode === 'rgb') {
+        } else if (drawMode === 'rgb' && isMouseDown === true) {
             rgb(e);
         } else {
-            alert('Please select a draw option')
+            return;
         };
     });
 };
-
-
