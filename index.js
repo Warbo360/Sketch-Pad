a9819d8f0a8490a47e019e5b65decacf27fbd6f3
Warbo360 <wbetz360@gmail.com>
2023-08-19 04:14:14 -0500

    Added a canvas resizing functionality
    Struggled with this part the most as I wasnt realizing that after I
    remove the already present canvas that I need to reselect all the canvas
    divs again with another querySelectorAll statement so that the evenListe
    ners could actually see the new divs that were created.
    
    Users can now select the Canvas Size button to input a new option that
    will then remove the old canvas, create a new one, querySelectorAll the
    new divs of the new canvas, and applies new evenListeners on each one
    for the draw modes.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Need a function that takes button input of size dimensions and outputs a .createElements() loop and iterates n times n being the axb dimenstions of the sketch pad, each 
 // draw box will be its own div, so flexbox with flex-wrap needed when creating the divs with JS manip 
 
+let singleBox = document.querySelectorAll('.singleBox'); // selecting the divs in the drawing area
+
 function createCanvas(sizeInput) {
     let canvasContainer = document.querySelector('.rightSideSketchZone');
     for (let i = 0; i < (sizeInput * sizeInput); i++) {
@@ -10,12 +12,49 @@ function createCanvas(sizeInput) {
         individualBox.style.height = 600/sizeInput + 'px';
 
         canvasContainer.appendChild(individualBox);
-    }
-}
+    };
+    singleBox = document.querySelectorAll('.singleBox'); // selecting the divs in the drawing area
+    // if isMouseDown = true, then the for loop fills in a div that has the mouse over it allows to press and hold draw functionality and stops if the button is not held
+    for (i = 0; i < singleBox.length; i++ ) {
+        singleBox[i].addEventListener('pointerover', function(e) {
+            if (drawMode === 'draw' && isMouseDown === true) {
+                draw(e);
+            } else if (drawMode === 'erase' && isMouseDown === true) {
+                erase(e);
+            } else if (drawMode === 'rgb' && isMouseDown === true) {
+                rgb(e);
+            } else {
+                return;
+            };
+        });
+    };
+
+    // Same as the above loop but changed the event to just a click, and removed the '&& isMouseDown conditional that way clicks in the div fill that individual div as well
+    for (i = 0; i < singleBox.length; i++ ) {
+        singleBox[i].addEventListener('click', function(e) {
+            if (drawMode === 'draw') {
+                draw(e);
+            } else if (drawMode === 'erase') {
+                erase(e);
+            } else if (drawMode === 'rgb') {
+                rgb(e);
+            } else {
+                return;
+            };
+        });
+    };
+};
 
 createCanvas(prompt("Input width x height number up to 100 please")); // Calling the above function with the input being a prompt to generate the sketch grid
 
-let singleBox = document.querySelectorAll('.singleBox'); // selecting the divs in the drawing area
+function removeCanvas() {
+    for (let i = 0; i < singleBox.length; i++) {
+        let box = singleBox[i];
+        box.remove();
+    }
+}
+
+
 let sketchSize = document.querySelectorAll('.sketchSize'); // selecting the div that will eventaully hold the various draw modes
 let gridLinesButton = document.querySelector('#gridlines'); // selecting the gridlines button
 let drawMode; // variable that is used to set the drawmode, and if clear is selected loops over each div coloring each white
@@ -41,6 +80,10 @@ sketchSize.forEach(((element) => element.addEventListener('click', function(){
             singleBox[i].style.backgroundColor = 'white';
         };
     };
+    if (drawMode === 'canvas size') {
+        removeCanvas();
+        createCanvas(prompt("Input width x height number up to 100 please"));
+    }
 })));
 
 // all of the draw modes, random integer function added to generate the random color for the rgb method
