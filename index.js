f861acd4112246c40dc7b23335f7ccf1fecb9eb0
Warbo360 <129965715+Warbo360@users.noreply.github.com>
2023-08-30 03:16:07 -0500

    Finished styling the page overall.
    Also added JS functionality that adds active response when a draw mode is selected inverting the color of the text and button background to show that is the active button currently, does not effect the clear, gridlines, and canvas button. Also removed the 'canvasSize' div as I ended up not really needed it. In the future if I wasnt to add a canvas size slider I'd just put it below the actual canvas I think. Also was able to add the shading draw option that progressively shades a div by 10% for each interaction with that div (just white to black). This required having the JS actively set the background color of the divs to white each times a canvas is made since thats what the conditional initially looks for at step one of the shading.

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function createCanvas(sizeInput) {
     singleBox = document.querySelectorAll('.singleBox'); // selecting the divs in the drawing area
     // if isMouseDown = true, then the for loop fills in a div that has the mouse over it allows to press and hold draw functionality and stops if the button is not held
     for (i = 0; i < singleBox.length; i++ ) {
+        singleBox[i].style.backgroundColor = 'white';
+    };
+    for (i = 0; i < singleBox.length; i++ ) {
         singleBox[i].addEventListener('pointerover', function(e) {
             if (drawMode === 'draw' && isMouseDown === true) {
                 draw(e);
@@ -73,7 +76,7 @@ gridLinesButton.addEventListener('click', function() {
 
 
 // Event listener to set draw modes based on buttons selctions and a for loop if the clear option is selected colors all the canvas divs white
-sketchSize.forEach(((element) => element.addEventListener('click', function(){
+sketchSize.forEach(((element) => element.addEventListener('click', function(e) {
     drawMode = element.innerText.toLowerCase();
     if (drawMode === 'clear') {
         for (i = 0; i < singleBox.length; i++ ) {
@@ -84,9 +87,41 @@ sketchSize.forEach(((element) => element.addEventListener('click', function(){
         removeCanvas();
         createCanvas(prompt("Input width x height number up to 100 please"));
     }
+    if (drawMode === 'draw') {
+        sketchSize.forEach((element) => element.style.color = 'black');
+        sketchSize.forEach((element) => element.style.backgroundColor = 'white')
+        draw(e);
+        activeWordColor(e);
+    } else if (drawMode === 'erase') {
+        sketchSize.forEach((element) => element.style.color = 'black');
+        sketchSize.forEach((element) => element.style.backgroundColor = 'white')
+        draw(e);
+        activeWordColor(e);
+    } else if (drawMode === 'rgb') {
+        sketchSize.forEach((element) => element.style.color = 'black');
+        sketchSize.forEach((element) => element.style.backgroundColor = 'white')
+        draw(e);
+        activeWordColor(e);
+    } else if (drawMode === 'shading') {
+        sketchSize.forEach((element) => element.style.color = 'black');
+        sketchSize.forEach((element) => element.style.backgroundColor = 'white')
+        draw(e);
+        activeWordColor(e);
+    } else {
+        sketchSize.forEach((element) => element.style.color = 'black');
+        sketchSize.forEach((element) => element.style.backgroundColor = 'white')
+    };
 })));
 
 // all of the draw modes, random integer function added to generate the random color for the rgb method
+function inactiveWordColor(e) {
+    e.target.style.color = 'black'
+};
+
+function activeWordColor(e) {
+    e.target.style.color = '#e6e6e6'
+};
+
 function draw(e) {
     e.target.style.backgroundColor = 'black';
 };
@@ -102,6 +137,32 @@ function getRandomInteger(max) {
 function rgb(e) {
     e.target.style.backgroundColor = 'rgb' + '(' + getRandomInteger(255) + ',' + getRandomInteger(255) + ',' + getRandomInteger(255) + ')';
 };
+
+function progressiveShading(e) {
+    if (e.target.style.backgroundColor === 'white') {
+        e.target.style.backgroundColor = 'rgb(230, 230, 230)';
+    } else if (e.target.style.backgroundColor === 'rgb(230, 230, 230)') {
+        e.target.style.backgroundColor = 'rgb(204, 204, 204)';
+    } else if (e.target.style.backgroundColor === 'rgb(204, 204, 204)') {
+        e.target.style.backgroundColor = 'rgb(179, 179, 179)';
+    } else if (e.target.style.backgroundColor === 'rgb(179, 179, 179)') {
+        e.target.style.backgroundColor = 'rgb(153, 153, 153)';
+    } else if (e.target.style.backgroundColor === 'rgb(153, 153, 153)') {
+        e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    } else if (e.target.style.backgroundColor === 'rgb(128, 128, 128)') {
+        e.target.style.backgroundColor = 'rgb(102, 102, 102)';
+    } else if (e.target.style.backgroundColor === 'rgb(102, 102, 102)') {
+        e.target.style.backgroundColor = 'rgb(77, 77, 77)';
+    } else if (e.target.style.backgroundColor === 'rgb(77, 77, 77)') {
+        e.target.style.backgroundColor = 'rgb(51, 51, 51)';
+    } else if (e.target.style.backgroundColor === 'rgb(51, 51, 51)') {
+        e.target.style.backgroundColor = 'rgb(26, 26, 26)';
+    } else if (e.target.style.backgroundColor === 'rgb(26, 26, 26)') {
+        e.target.style.backgroundColor = 'rgb(0, 0, 0)';
+    } else {
+        return;
+    }
+}
 
 // defining the function to draw and remove the gridlines of the drawing canvas
 function drawGridLines() {
@@ -136,6 +197,8 @@ for (i = 0; i < singleBox.length; i++ ) {
             erase(e);
         } else if (drawMode === 'rgb' && isMouseDown === true) {
             rgb(e);
+        } else if (drawMode === 'shading' && isMouseDown === true) {
+            progressiveShading(e);
         } else {
             return;
         };
@@ -151,6 +214,8 @@ for (i = 0; i < singleBox.length; i++ ) {
             erase(e);
         } else if (drawMode === 'rgb') {
             rgb(e);
+        } else if (drawMode === 'shading') {
+            progressiveShading(e);
         } else {
             return;
         };
